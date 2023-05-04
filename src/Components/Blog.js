b0ddce6e1ca71c7941560c34f4c5adabaf9e49a2
lannam <lannam@islander.tamucc.edu>
2023-05-04 16:49:42 -0500

    Initial Commit

---
+++ src/Components/Blog.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import "./Blog.css";
+
+
+
+function Blog() {
+
+  return (
+    <div className='page' >
+      <h1>Hello, I am Gayathri Annam</h1>
+      <h3>Master of Computer Science Student at Texas A&M University</h3>
+      <p>I have completed my two-year master's program (2021-2023) at the university and served as the Vice-President of the Indian Student Association TAMUCC from 2022 to 2023. I would like to share my overall university experience to benefit upcoming students.
+      </p>
+      <h2>
+      Have you heard about ISA and how it works? 
+      </h2>
+
+      <p>
+      It's an amazing organization completely focused on helping Indian students. ISA has a committee of students attending the university, and they change every year after the elections. The committee is supervised by Professor Dr. Mamta Yadav.
+      </p>
+      <h2>After receiving admission, what are the steps to follow?</h2>
+      <p>
+      Moving to a new country can be tough, but don't worry, this place is super friendly! As soon as you decide to join our university, the first thing to do is check out ISA TAMUCC Instagram page. Before the start of each semester, they post a WhatsApp group link for students who are joining TAMUCC. In this group, you can connect with seniors and ISA committee members who will answer any doubts you have. ISA is here to help you from start to end!
+      </p>
+      <h3>Accommodation & PickUps:</h3>
+      <p>When you join, the ISA team will ask you about your accommodation preferences and who you want to room with. Don't worry if you don't know anyone yet, ISA will help you find a group.
+
+Once you've confirmed that you're joining the university, ISA will book an apartment for you before your arrival so you can move in hassle-free. Most Indian students stay in The Shores community, Plam's, and South Bay, which are all close to the university. Plus, you'll have access to free bus transportation with your college student ID.
+
+To help with your arrival, ISA will share a Google sheet where you can provide your flight information. Someone from the ISA committee will then pick you up from the Corpus Christi airport or bus stop and drop you off at your apartment.
+
+Best of all, these accommodations and pickups are completely free! ISA has funding to cover these expenses, so you don't have to worry about any extra charges.
+</p>
+<h3>Academics & Courses</h3>
+<p>The university I attended is really great! For the Course Only Option, you need to complete at least 36 credit hours. There are 3 core courses that everyone has to take, and then you get to choose from electives in 3 concentration tracks. You need to complete at least 2 electives in each track before you can complete your masters. And, in your last semester, there's a mandatory course called "Advanced Software Engineering" where you'll be working on a capstone project. It's pretty exciting!</p>
+<p> for more details check out the link; https://catalog.tamucc.edu/graduate/engineering/masters/computer-science-ms/#programrequirementstext</p>
+<p> 
+Based on my experience, I found all the subjects to be enjoyable. There were a lot of assignments, projects, presentations, and submissions, but by the end of my masters, I learned a lot. Most of the courses required us to give presentations on project selection, project status updates, and final project presentations. Due to so many presentations, I lost stage fear, gained confidence, and improved my interpersonal skills. Some courses even conducted pen and paper tests. The majority of the classes were in-person, and attendance was mandatory. Typically, for each course, there were two to three classes per week, each lasting one hour.
+
+Course registration can be a bit hectic, like booking a Tatkal train ticket in India. You need to be very quick during the registration process. Find out when the courses will be released and go through the catalog beforehand to decide which courses you want to take. Then, be ready with your laptop and register for the courses at the right time. If you don't find any available courses, you can contact the ISA committee. They will provide you with the email address of the College of Engineering Dean, who you can contact to inquire about the courses you are interested in. </p>
+
+<h3>On Campus Employments (GA/TA/RA/Student Workers)</h3>
+<p>Handshake, dining link, ta link</p>
+    </div>
+  );
+}
+
+export default Blog;
